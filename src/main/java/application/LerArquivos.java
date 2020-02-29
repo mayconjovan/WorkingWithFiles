a265bcd6df907d1bdaee32fd11b9e897605393f2
Maycon J Pereira <mayconjovan@gmail.com>
2020-02-29 19:01:10 -0300

    BufferedWritter and Reader

--- src/main/java/application/LerArquivos.java
+++ src/main/java/application/LerArquivos.java
@@ -1,24 +1,24 @@
 package application;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class LerArquivos {
 
     public static void main(String[] args) {
 
-        String path = "c:\\temp\\in.txt";
-        
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line = br.readLine();
-
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
+        String[] lines = new String[] {"Good morning", "Good afternoon", "Good Night"};
+        String path = "c:\\temp\\out.txt";
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
             }
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+        }catch (IOException e) {
+            e.printStackTrace();
         }
+        
     }
 }
